109b66d2e3569d40c7da02a55c1ced230769204e
jhazelton <36609942+jhazelton@users.noreply.github.com>
2025-09-28 12:42:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,10 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
