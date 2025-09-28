09f99360b71081f9105d98b682c5d8db5daa69b4
jhazelton <36609942+jhazelton@users.noreply.github.com>
2025-09-28 14:50:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,6 +55,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
+#
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
