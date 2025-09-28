14fa60d4c29afd7313a05ed8a02f50f133d43b85
jhazelton <36609942+jhazelton@users.noreply.github.com>
2025-09-28 14:01:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,16 +16,10 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
-}
-
-data "aws_caller_identity" "current" {}
-
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
 }
 
