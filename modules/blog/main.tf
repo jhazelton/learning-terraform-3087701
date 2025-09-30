2f1398f34514bf2bee71b185ca2a3fcaa568bb9d
jhazelton <36609942+jhazelton@users.noreply.github.com>
2025-09-29 22:07:57 -0400

    Update main.tf

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -31,21 +31,6 @@ module "blog_vpc" {
 }
 
 
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-
-  name = "${var.environment.name}-blog"
-
-  min_size            = var.asg_min
-  max_size            = var.asg_max
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
-}
-
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
